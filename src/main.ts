62e1f3759db7f8f74fda920770faa3f0ca49bf66
CraftingDragon007 <info@craftingdragon.ch>
2026-04-28 18:53:19 +0200

    Update dependencies for stable release

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,12 @@
 import App from "./components/App.svelte";
+import { mount } from "svelte";
 
 const target = document.getElementById("app");
 if (!target) {
 	throw new Error("Could not find app root element");
 }
 
-const app = new App({
+const app = mount(App, {
 	target
 });
 
